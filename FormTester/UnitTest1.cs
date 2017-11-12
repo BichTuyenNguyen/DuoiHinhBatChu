b439adb7ae97eca516397efa1b9cf2663496856d
BichTuyenNguyen <bichtuyen6897@gmail.com>
2017-11-13 01:35:50 +0700

    trying test

--- FormTester/UnitTest1.cs
+++ FormTester/UnitTest1.cs
@@ -1,14 +1,55 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using 
-namespace FormTester
+using KiemThuPM;
+using System.Windows.Forms;
+namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest1
     {
+        // test form SignIn
+        public FrmSignIn signIn;
         [TestMethod]
-        public void TestMethod1()
+        public void TestSignIn_True()
         {
-        }
+            signIn = new FrmSignIn();
+            bool expected = true;
+            Assert.AreEqual(expected, signIn.KiemTraDangNhap("tuyen","tuyen"));
+
+        }//tên đăng nhập, mật khẩu đúng
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestSignIn_False_Pass()
+        {
+            signIn = new FrmSignIn();
+            signIn.KiemTraDangNhap("tuyen", "1");
+        }// tên đăng nhập đúng , mật khẩu sai
+        
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestSignIn_False_User()
+        {
+            signIn = new FrmSignIn();
+            signIn.KiemTraDangNhap("an", "1111");
+        }// tên đăng nhập sai, mật khẩu đúng
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestSignIn_False()
+        {
+            signIn = new FrmSignIn();
+            signIn.KiemTraDangNhap("an", "1");
+        }// tên đăng nhập sai , mật khẩu sai
+
+        //test form SignUp
+        public FrmSignUp signUp;
+        [TestMethod,Ignore]
+        public void TestSignUp_True()
+        {
+            signUp = new FrmSignUp();
+            bool expected = true;
+            Assert.AreEqual(expected, signUp.KiemTraDangKy("concho", "concho","cho"));
+
+        }//tên đăng nhập, mật khẩu đúng --> muốn test lại, phải đổi user
     }
 }
